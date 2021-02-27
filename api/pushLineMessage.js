15005d51ba633184185860dda9535f6e73c7bf73
azusaw <72424558+azusaw@users.noreply.github.com>
2021-02-27 22:57:55 +0900

    Create pushLineMessage.js

---
+++ api/pushLineMessage.js
@@ -0,0 +1,21 @@
+const line = require('@line/bot-sdk');
+
+exports.handler = async (event) => {
+
+    const client = new line.Client({
+      channelAccessToken: 'your-line-access-token'
+    });
+
+    const message = {
+      type: 'text',
+      text: `${event.message}};
+
+    await client.pushMessage(event.userId, message)
+      .then(() => {
+        console.log('send message')
+      })
+      .catch((err) => {
+        console.log('error: ', err)
+      });
+
+};
