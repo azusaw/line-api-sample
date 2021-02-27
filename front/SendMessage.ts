f599b77545f01c784b10865b4a9d2a8c3f9a6a55
azusaw <72424558+azusaw@users.noreply.github.com>
2021-02-28 00:10:11 +0900

    Update SendMessage.ts

--- front/SendMessage.ts
+++ front/SendMessage.ts
@@ -1,9 +1,48 @@
 import React, { useState } from "react"
 
+type LineInfo = {
+  userName: string
+  userId: string
+  picture: string
+  isFriend: boolean
+}
+
+type Audience = {
+  [key: string]: boolean
+}
+
 const Example = () => {
-  const SendNarrowMessage = async () => {
+  /* If you want to create access token(JWT Token) by your self, don't define */
+  const CHANNEL_ACCESS_TOKEN = "your-access-token"
+  
+  const SendMessage = async (message: string, lineInfo: LineInfo) => {
+    /* attention: It is channel access token, NOT user accsess token */
     const formData = {
-      audience: checkedAudienceList,
+      chanelAccessToken: CHANNEL_ACCESS_TOKEN,
+      userId: lineInfo.userId,
+      userName: lineInfo.userName,
+    }
+    fetch(
+      "https://hogehoge.execute-api.ap-your-region.amazonaws.com/default/pushLineMessage",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      }
+    )
+    .then((response) => {
+      console.log("success")
+    })
+    .catch((e) => {
+      console.error(e)
+    })
+  }
+
+  
+  const SendNarrowMessage = async (message: string, audienceList: Audience[]) => {
+    const formData = {
+      audience: audienceList,
       messages: messages,
     }
     fetch(
